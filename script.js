7989f4315a8ed2c098e833799eda620090e982c4
grant h <huiras.grant@gmail.com>
2022-10-13 21:43:42 -0500

    Wrote button click & answer correct functions.

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const qContainerElement = document.getElementById("question-container");
+const qElement = document.getElementById("q");
+const aElement = document.getElementById("a");
+const comBtn = document.getElementById("com-btn");
+const nxtBtn = document.getElementById("nxt-btn");
+
 var time = 40;
 
 function setTimer() {
@@ -10,8 +16,38 @@ function setTimer() {
     }), 1000
 };
 
-const qContainerElement = document.getElementById("question-container");
-const qElement = document.getElementById("q");
-const aElement = document.getElementById("a");
-const comBtn = document.getElementById("com-btn");
-const nxtBtn = document.getElementById("nxt-btn");
+let shuffleQ, currentQIndex;
+
+comBtn.addEventListener('click', startQuiz);
+nxtBtn.addEventListener('click', () => {
+    currentQIndex++
+    nxtQ()
+});
+
+function startQuiz () {
+    comBtn.classList.add('hide'),
+    shuffleQ = questions.sort(() => Math.random() - .5)
+    currentQIndex = 0
+    qContainerElement.classList.remove('hide')
+    nxtQ()
+};
+
+function nxtQ() {
+    showQ(shuffleQ[currentQIndex]),
+    resetState()
+};
+
+function showQ(question) {
+    qElement.innertext = question.question,
+    question.answers = forEach(answer => {
+        const btn = document.createElement('button')
+        btn.innertext = answer.text,
+        btn.classList.add('button')
+        if (answer.correct) {
+            btn.dataset.correct = answer.correct
+        }
+        btn.addEventListener('click', selectA)
+        aElement.appendChild(btn)
+    })
+};
+
